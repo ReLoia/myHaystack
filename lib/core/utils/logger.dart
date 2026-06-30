c67a23f5cd3de2ce0415a67783d577e82066a5d1
ReLoia <zrennyz@gmail.com>
2026-06-30 19:47:00 +0200

    refactor: update Logger utility to use debugPrint

--- lib/core/utils/logger.dart
+++ lib/core/utils/logger.dart
@@ -2,20 +2,14 @@ import 'package:flutter/foundation.dart';
 
 class Logger {
   static void info(String message, {String prefix = "APP"}) {
-    if (kDebugMode) {
-      print("[$prefix] INFO: $message");
-    }
+    debugPrint("[$prefix] INFO: $message");
   }
 
   static void error(String message, {String prefix = "APP", dynamic error}) {
-    if (kDebugMode) {
-      print("[$prefix] ERROR: $message${error != null ? ' - $error' : ''}");
-    }
+    debugPrint("[$prefix] ERROR: $message${error != null ? ' - $error' : ''}");
   }
 
   static void debug(String message, {String prefix = "APP"}) {
-    if (kDebugMode) {
-      print("[$prefix] DEBUG: $message");
-    }
+    debugPrint("[$prefix] DEBUG: $message");
   }
 }
